7a90a5912ad6fd9392650eeedd0d7151be1de7dd
Igor Bari <mucsi96@gmail.com>
2024-07-19 15:12:32 +0200

    create repo index

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 brew update && brew upgrade && brew install kubernetes-cli
+
+pip install --upgrade pip
+
+pip install -r requirements.txt
--- scripts/publish_charts.py
+++ scripts/publish_charts.py
@@ -14,14 +14,15 @@ makedirs(root_directory / "dist", exist_ok=True)
 for package in [
     {"tag-prefix": "client-app", "src": root_directory / "charts/client_app"},
 ]:
+    latest_verison = version_utils.get_latest_version(package["tag-prefix"])
     version = version_utils.get_version(
         src=package["src"], tag_prefix=package["tag-prefix"]
     )
     
     with open(package["src"] / "Chart.yaml", "r") as file:
         chart_yaml = file.read()
-        chart_yaml = chart_yaml.replace("version: 0.1.0", f"version: {version}.0.0")
-        
+        chart_yaml = chart_yaml.replace("version: 0.1.0", f"version: {version or latest_verison}.0.0")
+
     with open(package["src"] / "Chart.yaml", "w") as file:
         file.write(chart_yaml)
 
@@ -36,21 +37,32 @@ for package in [
         cwd=root_directory,
         check=True,
     )
-    github_utils.create_release(
-        tag_prefix=package["tag-prefix"],
-        version=version,
-        access_token=sys.argv[1],
-        body=dedent(
-            f"""
-            [Helm chart](https://mucsi96.github.io/k8s-helm-charts/{package["tag-prefix"]}-{version}.0.0.tgz)
+    if version:
+        github_utils.create_release(
+            tag_prefix=package["tag-prefix"],
+            version=version,
+            access_token=sys.argv[1],
+            body=dedent(
+                f"""
+                [Helm chart](https://mucsi96.github.io/k8s-helm-charts/{package["tag-prefix"]}-{version}.0.0.tgz)
 
-            ```bash
-                helm repo add mucsi96 https://mucsi96.github.io/k8s-helm-charts
-                helm install mucsi96/{package["tag-prefix"]} --version {version}.0.0
-            ```
-        """
-        ),
-    )
+                ```bash
+                    helm repo add mucsi96 https://mucsi96.github.io/k8s-helm-charts
+                    helm install mucsi96/{package["tag-prefix"]} --version {version}.0.0
+                ```
+            """
+            ),
+        )
 
+run(
+    [
+        "helm",
+        "repo",
+        "index",
+        "dist",
+    ],
+    cwd=root_directory,
+    check=True,
+)
 
 github_utils.create_pages_artifact(directory=root_directory / "dist")
