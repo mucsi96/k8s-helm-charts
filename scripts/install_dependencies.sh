1bfd50b067473cfa84778161a8da4e627de685a4
Igor Bari <mucsi96@gmail.com>
2024-07-21 10:19:59 +0200

    add envFrom

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,5 @@ brew update && brew upgrade && brew install kubernetes-cli
 pip install --upgrade pip
 
 pip install -r requirements.txt
+
+helm repo add mucsi96 https://mucsi96.github.io/k8s-helm-charts
