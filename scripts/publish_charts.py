f66402e7f316ba4a7098534dd550f8fbce899e15
Igor Bari <mucsi96@gmail.com>
2024-07-26 08:23:34 +0200

    publish all charts

--- scripts/publish_charts.py
+++ scripts/publish_charts.py
@@ -13,15 +13,19 @@ makedirs(root_directory / "dist", exist_ok=True)
 
 for package in [
     {"tag-prefix": "client-app", "src": root_directory / "charts/client_app"},
+    {"tag-prefix": "spring-app", "src": root_directory / "charts/spring_app"},
+    {"tag-prefix": "node-app", "src": root_directory / "charts/node_app"},
 ]:
     latest_verison = version_utils.get_latest_version(package["tag-prefix"])
     version = version_utils.get_version(
         src=package["src"], tag_prefix=package["tag-prefix"]
     )
-    
+
     with open(package["src"] / "Chart.yaml", "r") as file:
         chart_yaml = file.read()
-        chart_yaml = chart_yaml.replace("version: 0.1.0", f"version: {version or latest_verison}.0.0")
+        chart_yaml = chart_yaml.replace(
+            "version: 0.1.0", f"version: {version or latest_verison}.0.0"
+        )
 
     with open(package["src"] / "Chart.yaml", "w") as file:
         file.write(chart_yaml)
