1580690c4583aa206552ea1af9d2e83b3a12fe86
Igor Bari <mucsi96@gmail.com>
2025-01-12 09:21:45 +0100

    update install_dependencies.sh for OS-specific kubectl and helm checks; change keyvault name in pull_kubeconfig.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
 
-brew update && brew upgrade && brew install kubernetes-cli
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    brew update && brew upgrade && brew install kubernetes-cli
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if ! command -v kubectl &> /dev/null; then
+        echo "kubectl is not installed. Please install kubectl and try again."
+        exit 1
+    fi
+
+    if ! command -v helm &> /dev/null; then
+        echo "helm is not installed. Please install helm and try again."
+        exit 1
+    fi
+fi
 
 pip install --upgrade pip
 
